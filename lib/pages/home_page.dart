d3cd25e66673cdbcdc6619fbca80e2062afafe40
ildefonsocb13 <ildefonsocb13@gmail.com>
2023-04-18 18:42:34 -0600

    second commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key});
@@ -8,6 +9,7 @@ class HomePage extends StatelessWidget {
 
   //SignUserOut
   void signUserOut() {
+    GoogleSignIn().signOut();
     FirebaseAuth.instance.signOut();
   }
 
